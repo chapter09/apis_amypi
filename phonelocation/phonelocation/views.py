37ceada2c89dcb304688edc7e79d27f6ea7187cd
chapter09 <wh.sjtu@gmail.com>
2013-11-03 16:48:56 +0800

    update phonelocation api

--- phonelocation/phonelocation/views.py
+++ phonelocation/phonelocation/views.py
@@ -6,33 +6,21 @@ from django.views.generic import View
 from django.shortcuts import render_to_response
 import urllib2, json
 
-keys = ['gid', 'name', 'todayStartPri', 'yestodEndPri', 'nowPri', 'todayMax', 'todayMin', 'competitivePri', 'reservePri', 'traNumber', 'traAmount', 'buyOne', 'buyOnePri', 'buyTwo', 'buyTwoPri', 'buyThree', 'buyThreePri', 'buyFour', 'buyFourPri', 'buyFive', 'buyFivePri', 'sellOne', 'sellOnePri', 'sellTwo', 'sellTwoPri', 'sellThree', 'sellThreePri', 'sellFour', 'sellFourPri', 'sellFive', 'sellFivePri', 'date', 'time']
-
-
 def query(request):
     #print request 
-    #if :
-        # 404 address not found
 
-    #r = urllib2.urlopen('http://hq.sinajs.cn/list=%s'%param)
-    values = []
     tel = request.GET.get('tel').strip()
     # input check
-
-
-    values.append(tel)
-
-    r = urllib2.urlopen('http://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=%s'%tel)
-    #html = r.read().decode('gb18030').encode('utf-8')
-    html = r.read().decode('gb18030')
-    
-    # process those strings
-
-    values.extend(html.split('=')[1][1:-3].split(','))
-
-    data = {"result":[{"data":dict(zip(keys, values))}]}
-
-
-    # return json object
-    return HttpResponse(json.dumps(data, ensure_ascii=False), mimetype='application/json')
+    if tel.isdigit() and len(tel) == 11:
+        r = urllib2.\
+                urlopen('http://cz.115.com/?ct=index&ac=get_mobile_local&mobile=%s'%tel)
+        html = r.read()
+        data = json.loads(html[1:-1].strip())
+        
+        if data['queryresult'] == "True":
+            return HttpResponse(json.dumps(data, ensure_ascii=False), mimetype='application/json')
+        else:
+            return HttpResponse("The phone number does not exist.", mimetype='text/plain')
+    else:
+        return HttpResponse("Please enter a valid phone number!", mimetype='text/plain')
 
